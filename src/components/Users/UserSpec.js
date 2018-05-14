a7c94ed8f887f78262083a850ebe89b20ed66211
quandv <quanduongvan@admicro.vn>
2018-05-14 22:08:36 +0700

    test git

--- src/components/Users/UserSpec.js
+++ src/components/Users/UserSpec.js
@@ -3,7 +3,7 @@ import * as config from './../../constants/config';
 import { Link } from 'react-router-dom';
 
 class UserSpec extends Component {
-	
+	//test
 	onDelete (id) {
 		this.props.onDelete(id);
 	}
