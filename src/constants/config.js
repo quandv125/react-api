65ba1c6217a1c49c7fb24e95f5937b829e1badd9
Quan <quandv.125@gmail.com>
2018-07-22 10:06:58 +0700

    test1

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
-//Api
+//API 
 export const APP_URL = 'http://127.0.0.1:8000/api/v1/user';
 export const LOGIN_URL = 'http://127.0.0.1:8000/api/v1/login';
 export const LOGOUT_URL = 'http://127.0.0.1:8000/api/v1/logout?token=';
